4387db83d4058e886071678d11ff10a205eda640
Allison Lyon <alyoness1@gmail.com>
2016-11-16 23:52:23 -0800

    ugh

--- supertable/src/app/app.component.ts
+++ supertable/src/app/app.component.ts
@@ -11,13 +11,41 @@ const SECTIONS = require('./data-schema')
 // const people_subject = new BehaviorSubject<Person[]>([]);
 
 
-
+interface PersonData {
+    name: {
+        prefix?: string,
+        first?: string,
+        last?: string,
+    },
+    features: {
+        gender?: string, 
+        height?: number,
+        weight?: number,
+        age?: number,
+        has_nose?: boolean,
+    }
+};
 
 export class Person {
-    constructor(
-        public attributes: Object
-    ) {
+    
+    static fromData(data: PersonData): Person {
+        let person = Object.create(Person.prototype);
+        return Object.assign(person, data, {
+            prefix: data.name.prefix ? data.name.prefix : null,
+            first: data.name.first ? data.name.first :null,
+            last: data.name.last ? data.name.last: null,
+            gender: data.features.gender ? data.features.gender : null,
+            height: data.features.height ? data.features.height: null,
+            weight: data.features.weight ? data.features.weight : null,
+            age: data.features.age ? data.features.age : null,
+            has_nose: data.features.has_nose ? data.features.has_nose : null,
+        });
     }
+
+    constructor(
+        public name: Object,
+        public features: Object
+    ) {}
 
     mapAttrs(fn) {
         Object.keys(this.attributes)
